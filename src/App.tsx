3f6bfef1bad19f233a8e39c8798633a204c0b242
Mr.Choi <cks.egg@gmail.com>
2023-03-03 15:13:06 +0900

    graphql 코드 추가

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,49 @@
 import * as React from 'react';
 import ImageRow from "./components/ImagesRow";
 import {Grid} from "@mui/material";
+import { useQuery, gql } from '@apollo/client';
+import ImageItem from "./components/ImageItem";
 
+const GET_ALL_USERS = gql`
+    {userList {
+        id, name, email, age, 
+        orderList {
+            id, amount, 
+            item {
+                category, name, price
+            }
+        }
+    }}
+`;
+
+function DisplayUsers() {
+    const { loading, error, data } = useQuery(GET_ALL_USERS);
+    type User = {id: string, name: string, email: string, age: number, orderList: []}
+    type Order = {id: number, amount: number, item:Item}
+    type Item = {name: string, price: number}
+
+    if (loading) return <p>Loading...</p>;
+    if (error) return <p>Error : {error.message}</p>;
+
+    return data.userList.map(({ id, name, email, age, orderList } : User) => (
+        <div key={id}>
+            <h3>{name}</h3>
+            <p>{age}</p>
+            <p>{email}</p>
+            {orderList.length > 0 && orderList.map((order: Order) => {
+                return (
+                    <div key={order.id}>
+                        <p>상품 : {order.item.name}</p>
+                        <p>가격 : {order.item.price}</p>
+                        <p>수량 : {order.amount}</p>
+                        <p>총액 : {order.item.price * order.amount}</p>
+                    </div>
+                );
+            })}
+            <br />
+        </div>
+    ));
+}
 export default function App() {
     return (
         <Grid container spacing={2}>
@@ -11,7 +53,7 @@ export default function App() {
                 <ImageRow rowNumber={2} />
             </Grid>
             <Grid item >
-                <div>세부 디테일 설정</div>
+                <DisplayUsers />
             </Grid>
         </Grid>
     );
--- src/index.tsx
+++ src/index.tsx
@@ -6,16 +6,24 @@ import App from './App';
 import theme from './theme';
 import store from './app/store';
 import {Provider} from "react-redux";
+import { ApolloClient, InMemoryCache, ApolloProvider, gql } from '@apollo/client';
 
 const rootElement = document.getElementById('root');
 const root = createRoot(rootElement!);
 
+const client = new ApolloClient({
+    uri: 'http://localhost:8080/api',
+    cache: new InMemoryCache(),
+});
+
 root.render(
-    <ThemeProvider theme={theme}>
-        {/* CssBaseline kickstart an elegant, consistent, and simple baseline to build upon. */}
-        <CssBaseline />
-        <Provider store={store}>
-            <App />
-        </Provider>
-    </ThemeProvider>,
+    <ApolloProvider client={client}>
+        <ThemeProvider theme={theme}>
+            {/* CssBaseline kickstart an elegant, consistent, and simple baseline to build upon. */}
+            <CssBaseline />
+            <Provider store={store}>
+                <App />
+            </Provider>
+        </ThemeProvider>
+    </ApolloProvider>
 );
